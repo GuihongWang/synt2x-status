e6a833494115ec47ca1aa36382dca62808980dd6
Guihong Wang <marisa@marisa.ml>
2025-08-12 11:15:29 +0800

    1

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	"m801148054-13eead03ef78e549ad7b415f", 
+	"m801148091-11ca158e93c313bb371daf0d", 
+	"m801148095-400c1e5f4af0027c3b2a2feb", 
+	"m801148105-7f9c1ec200d59ae18324a604", 
+	"m801148101-a61e77be7da0ce8b8c5f4fd6", 
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
